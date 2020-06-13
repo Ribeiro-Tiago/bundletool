b9c8f5c347d0c5a2b68929b07078282fe9c1c9f4
Noah Prail <nprail@filiosoft.com>
2020-06-13 14:39:44 -0400

    feat: Move bundletool download to a install script

---
+++ bin.d.ts
@@ -0,0 +1,3 @@
+export declare const BIN_PATH: string;
+export declare const DEFAULT_BASE_DIRECTORY: string;
+export declare const BUNDLETOOL_FILE_PATH: string;
---
+++ install.js
@@ -0,0 +1,63 @@
+const { existsSync, mkdirSync, createWriteStream } = require("fs");
+const axios = require("axios");
+
+const pkg = require("./package.json");
+
+const BUNDLETOOL_VERSION = pkg.bundletool.version;
+
+const { DEFAULT_BASE_DIRECTORY, BUNDLETOOL_FILE_PATH } = require("./bin");
+const BUNDLETOOL_URL = `https://github.com/google/bundletool/releases/download/${BUNDLETOOL_VERSION}/bundletool-all-${BUNDLETOOL_VERSION}.jar`;
+
+const log = (message) => {
+  const logLevel = process.env.npm_config_loglevel;
+  const logLevelDisplay = ["silent", "error", "warn"].indexOf(logLevel) > -1;
+
+  if (!logLevelDisplay) {
+    console.log(message);
+  }
+};
+
+const getFileStream = () => {
+  if (!existsSync(DEFAULT_BASE_DIRECTORY)) {
+    mkdirSync(DEFAULT_BASE_DIRECTORY);
+  }
+
+  return createWriteStream(BUNDLETOOL_FILE_PATH);
+};
+
+const downloadBinary = async () => {
+  return new Promise(async (resolve, reject) => {
+    log(
+      `Downloading bundletool ${BUNDLETOOL_VERSION} to ${BUNDLETOOL_FILE_PATH}`,
+    );
+
+    const { data } = await axios.get(BUNDLETOOL_URL, {
+      responseType: "stream",
+      headers: {
+        "Cache-Control": "max-age=0",
+        Connection: "keep-alive",
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
+      },
+    });
+
+    data.pipe(getFileStream());
+
+    data.on("end", () => {
+      resolve(BUNDLETOOL_FILE_PATH);
+    });
+    data.on("error", (error) => {
+      reject(error);
+    });
+
+    return BUNDLETOOL_FILE_PATH;
+  });
+};
+
+const setupBundletool = async () => {
+  if (!existsSync(BUNDLETOOL_FILE_PATH)) {
+    await downloadBinary();
+  }
+};
+
+setupBundletool();
