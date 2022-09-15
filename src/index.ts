e0d2e0fcfaea862e11cb6f01d6afc86533dedbcf
Christian Zommerfelds <christian.zommerfelds@gmail.com>
2022-09-15 09:15:33 +0200

    Only check for the Java binary lazily

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,18 @@ import commandExists from 'command-exists';
 import { Command } from "./types";
 import { BUNDLETOOL_FILE_PATH } from "../constants";
 
-if (!commandExists.sync('java')) {
-  throw new Error("Java is required for bundletool");
+
+var checkedJava = false;
+function checkJavaInPath() {
+  if (!commandExists.sync('java')) {
+    throw new Error("Java is required for bundletool");
+  }
+  checkedJava = true;
 }
 
 export default async (command: Command, args: string[] = []) => {
+  if (!checkedJava) {
+    checkJavaInPath();
+  }
   return spawnSync("java", ["-jar", BUNDLETOOL_FILE_PATH, command, ...args]);
 };
